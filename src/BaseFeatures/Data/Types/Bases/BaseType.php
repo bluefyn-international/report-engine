316f8b0af5544c1b37be00b885f9cd1a6dfa39ef
Lucas Graciano <lucas.nexus@gmail.com>
2024-06-25 14:31:09 -0700

    BaseType: Add tooltip to renderFilter

--- src/BaseFeatures/Data/Types/Bases/BaseType.php
+++ src/BaseFeatures/Data/Types/Bases/BaseType.php
@@ -179,21 +179,35 @@ abstract class BaseType
      * @param array       $action_types
      * @param self        $columnType
      * @param Collection  $value
+     * @param ?string     $tooltip
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function renderFilter(string $label, string $name, array $action_types, self $columnType, Collection $value)
-    {
+    public function renderFilter(
+        string $label,
+        string $name,
+        array $action_types,
+        self $columnType,
+        Collection $value,
+        ?string $tooltip = null,
+    ) {
         return view($this->filterView)
             ->with(
-                $this->getConfig($label, $name, $action_types, $columnType, $value)
+                $this->getConfig($label, $name, $action_types, $columnType, $value, $tooltip)
             );
     }
 
-    public function getConfig(string $label, string $name, array $action_types, self $columnType, Collection $value) : array
-    {
+    public function getConfig(
+        string $label,
+        string $name,
+        array $action_types,
+        self $columnType,
+        Collection $value,
+        ?string $tooltip = null,
+    ) : array {
         return [
             'label' => $label,
+            'tooltip' => $tooltip,
             'field' => $name,
             'value' => $value,
             'action_types' => $action_types,
